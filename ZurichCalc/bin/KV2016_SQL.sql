c7d58947731e62d34744e3f61923133b27abcbaf
rauchenm <rauchenm@gmail.com>
2017-12-28 18:56:03 +0100

    First check in

---
+++ ZurichCalc/bin/KV2016_SQL.sql
@@ -0,0 +1,83 @@
+﻿CREATE TABLE `KV_2016` (
+	`Id` INT(11) NOT NULL AUTO_INCREMENT,
+	`Stufe` VARCHAR(50) NOT NULL,
+	`Wert` DOUBLE NOT NULL,
+	PRIMARY KEY (`Id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
+
+Insert into KV_2016 (Stufe, Wert) Values ('I/1', 24726.94);
+Insert into KV_2016 (Stufe, Wert) Values ('I/3', 27540.94);
+Insert into KV_2016 (Stufe, Wert) Values ('I/2', 26133.94);
+Insert into KV_2016 (Stufe, Wert) Values ('I/4', 28966.28);
+Insert into KV_2016 (Stufe, Wert) Values ('I/5', 30354.80);
+Insert into KV_2016 (Stufe, Wert) Values ('I/6', 32492.88);
+Insert into KV_2016 (Stufe, Wert) Values ('I/7', 34612.48);
+Insert into KV_2016 (Stufe, Wert) Values ('I/8', 35580.86);
+Insert into KV_2016 (Stufe, Wert) Values ('II/1', 26280.10);
+Insert into KV_2016 (Stufe, Wert) Values ('II/2', 27778.38);
+Insert into KV_2016 (Stufe, Wert) Values ('II/3', 29313.34);
+Insert into KV_2016 (Stufe, Wert) Values ('II/4', 30811.62);
+Insert into KV_2016 (Stufe, Wert) Values ('II/5', 32327.96);
+Insert into KV_2016 (Stufe, Wert) Values ('II/6', 34594.14);
+Insert into KV_2016 (Stufe, Wert) Values ('II/7', 36841.84);
+Insert into KV_2016 (Stufe, Wert) Values ('II/8', 37877.10);
+Insert into KV_2016 (Stufe, Wert) Values ('III/1', 28144.20);
+Insert into KV_2016 (Stufe, Wert) Values ('III/2', 29751.96);
+Insert into KV_2016 (Stufe, Wert) Values ('III/3', 31396.54);
+Insert into KV_2016 (Stufe, Wert) Values ('III/4', 33004.58);
+Insert into KV_2016 (Stufe, Wert) Values ('III/5', 34648.88);
+Insert into KV_2016 (Stufe, Wert) Values ('III/6', 37079.14);
+Insert into KV_2016 (Stufe, Wert) Values ('III/7', 39527.88);
+Insert into KV_2016 (Stufe, Wert) Values ('III/8', 40643.72);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/1', 29824.90);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/2', 31560.76);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/3', 33278.28);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/4', 35032.90);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/5', 36750.14);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/6', 39363.24);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/7', 41939.94);
+Insert into KV_2016 (Stufe, Wert) Values ('IV/8', 43128.14);
+Insert into KV_2016 (Stufe, Wert) Values ('V/1', 31853.22);
+Insert into KV_2016 (Stufe, Wert) Values ('V/2', 33717.04);
+Insert into KV_2016 (Stufe, Wert) Values ('V/3', 35581.00);
+Insert into KV_2016 (Stufe, Wert) Values ('V/4', 37444.68);
+Insert into KV_2016 (Stufe, Wert) Values ('V/5', 39290.44);
+Insert into KV_2016 (Stufe, Wert) Values ('V/6', 42067.76);
+Insert into KV_2016 (Stufe, Wert) Values ('V/7', 44863.28);
+Insert into KV_2016 (Stufe, Wert) Values ('V/8', 46139.18);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/1', 35050.96);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/2', 37097.20);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/3', 39143.72);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/4', 41227.06);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/5', 43274.00);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/6', 46361.84);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/7', 49431.62);
+Insert into KV_2016 (Stufe, Wert) Values ('VI/8', 50844.57);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/1', 38742.06);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/2', 41044.22);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/3', 43328.46);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/4', 45631.04);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/5', 47914.86);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/6', 51289.42);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/7', 54629.40);
+Insert into KV_2016 (Stufe, Wert) Values ('VII/8', 56198.28);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/1', 44589.02);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/2', 47256.86);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/3', 49893.34);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/4', 52473.54);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/5', 55018.32);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/6', 58888.06);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/7', 62741.00);
+Insert into KV_2016 (Stufe, Wert) Values ('VIII/8', 64553.23);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/1', 52473.54);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/2', 55513.08);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/3', 58552.62);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/4', 61627.72);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/5', 64667.26);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/6', 69244.14);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/7', 73803.38);
+Insert into KV_2016 (Stufe, Wert) Values ('IX/8', 75947.48);
---
+++ ZurichCalc/bin/KV2017_SQL.sql
@@ -0,0 +1,83 @@
+﻿CREATE TABLE `KV_2017` (
+	`Id` INT(11) NOT NULL AUTO_INCREMENT,
+	`Stufe` VARCHAR(50) NOT NULL,
+	`Wert` DOUBLE NOT NULL,
+	PRIMARY KEY (`Id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
+
+Insert into KV_2017 (Stufe, Wert) Values ('I/1', 25117.96);
+Insert into KV_2017 (Stufe, Wert) Values ('I/2', 26540.36);
+Insert into KV_2017 (Stufe, Wert) Values ('I/3', 27962.90);
+Insert into KV_2017 (Stufe, Wert) Values ('I/4', 29403.92);
+Insert into KV_2017 (Stufe, Wert) Values ('I/5', 30807.70);
+Insert into KV_2017 (Stufe, Wert) Values ('I/6', 32969.30);
+Insert into KV_2017 (Stufe, Wert) Values ('I/7', 35112.28);
+Insert into KV_2017 (Stufe, Wert) Values ('I/8', 36091.30);
+Insert into KV_2017 (Stufe, Wert) Values ('II/1', 26688.20);
+Insert into KV_2017 (Stufe, Wert) Values ('II/2', 28203.00);
+Insert into KV_2017 (Stufe, Wert) Values ('II/3', 29754.76);
+Insert into KV_2017 (Stufe, Wert) Values ('II/4', 31269.56);
+Insert into KV_2017 (Stufe, Wert) Values ('II/5', 32802.56);
+Insert into KV_2017 (Stufe, Wert) Values ('II/6', 35093.66);
+Insert into KV_2017 (Stufe, Wert) Values ('II/7', 37366.14);
+Insert into KV_2017 (Stufe, Wert) Values ('II/8', 38412.78);
+Insert into KV_2017 (Stufe, Wert) Values ('III/1', 28572.74);
+Insert into KV_2017 (Stufe, Wert) Values ('III/2', 30198.28);
+Insert into KV_2017 (Stufe, Wert) Values ('III/3', 31860.92);
+Insert into KV_2017 (Stufe, Wert) Values ('III/4', 33486.60);
+Insert into KV_2017 (Stufe, Wert) Values ('III/5', 35148.96);
+Insert into KV_2017 (Stufe, Wert) Values ('III/6', 37605.96);
+Insert into KV_2017 (Stufe, Wert) Values ('III/7', 40081.72);
+Insert into KV_2017 (Stufe, Wert) Values ('III/8', 41209.70);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/1', 30271.92);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/2', 32026.96);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/3', 33763.38);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/4', 35537.32);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/5', 37273.46);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/6', 39915.26);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/7', 42520.24);
+Insert into KV_2017 (Stufe, Wert) Values ('IV/8', 43721.58);
+Insert into KV_2017 (Stufe, Wert) Values ('V/1', 32322.64);
+Insert into KV_2017 (Stufe, Wert) Values ('V/2', 34206.90);
+Insert into KV_2017 (Stufe, Wert) Values ('V/3', 36091.44);
+Insert into KV_2017 (Stufe, Wert) Values ('V/4', 37975.56);
+Insert into KV_2017 (Stufe, Wert) Values ('V/5', 39841.62);
+Insert into KV_2017 (Stufe, Wert) Values ('V/6', 42649.46);
+Insert into KV_2017 (Stufe, Wert) Values ('V/7', 45475.78);
+Insert into KV_2017 (Stufe, Wert) Values ('V/8', 46765.74);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/1', 35555.52);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/2', 37620.30);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/3', 39693.36);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/4', 41799.52);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/5', 43869.00);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/6', 46990.86);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/7', 50094.38);
+Insert into KV_2017 (Stufe, Wert) Values ('VI/8', 51522.80);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/1', 39287.22);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/2', 41614.72);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/3', 43924.02);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/4', 46251.94);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/5', 48560.96);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/6', 51972.62);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/7', 55349.28);
+Insert into KV_2017 (Stufe, Wert) Values ('VII/8', 56935.48);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/1', 45198.44);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/2', 47895.68);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/3', 50561.14);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/4', 53169.76);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/5', 55742.54);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/6', 59654.84);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/7', 63550.20);
+Insert into KV_2017 (Stufe, Wert) Values ('VIII/8', 65382.38);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/1', 53169.76);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/2', 56242.76);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/3', 59315.76);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/4', 62424.60);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/5', 65497.60);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/6', 70124.88);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/7', 74734.24);
+Insert into KV_2017 (Stufe, Wert) Values ('IX/8', 76901.86);
